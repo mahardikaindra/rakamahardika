dea752a6cbe36d2e30026aef31bf2f3ca2d5eea7
Raka Mahardika <rakamahardika@Rakas-Mac-mini.local>
2024-08-19 13:50:55 +0700

    Base layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      {/* /* Create header navigation */}
+        <header className="bg-white py-4 sticky top-0 z-10">
+          <div className="container mx-auto flex justify-between items-center px-4">
+            <a href="/" className="text-black text-lg font-bold">Raka Mahardika</a>
+            <nav>
+            <ul className="flex space-x-4">
+          <li>
+            <a href="#about" className="text-black hover:text-[#757575]">About</a>
+          </li>
+          <li>
+            <a href="#education" className="text-black hover:text-[#757575]">Education</a>
+          </li>
+          <li>
+            <a href="#skills" className="text-black hover:text-[#757575]">Skills</a>
+          </li>
+          <li>
+            <a href="#experience" className="text-black hover:text-[#757575]">Experience</a>
+          </li>
+          <li>
+            <a href="#projects" className="text-black hover:text-[#757575]">Projects</a>
+          </li>
+            </ul>
+            </nav>
+          </div>
+        </header>
+        {/* Create banner section */}
+      <section className="bg-gray-900 py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-white text-4xl font-bold">{`Hello, I'm Raka Mahardika`}</h2>
+          <p className="text-gray-300 mt-4">{`I'm a software engineer based in Indonesia. I love to build things for the web.`}</p>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      {/* Create about section */}
+      <section id="about" className="bg-white py-16">
+        <div className="container mx-auto px-">
+          <h2 className="text-2xl font-bold text-right text-black">About Me</h2>
+          <p className="mt-4 text-right text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt consequatur fugiat, dolorum velit molestias sapiente odio tempore quisquam dolorem. Odio iusto, odit aut sapiente commodi consequuntur numquam nemo perspiciatis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt consequatur fugiat, dolorum velit molestias sapiente odio tempore quisquam dolorem. Odio iusto, odit aut sapiente commodi consequuntur numquam nemo perspiciatis.</p>
+        </div>
+      </section>
+      {/* Create education section */}
+      <section id="education" className="bg-gray-100 py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold">Education</h2>
+          <ul className="mt-4">
+            <li>
+              <h3 className="text-lg font-bold">Bachelor of Computer Science</h3>
+              <p>University of Widyatma</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      {/* Create skills section */}
+      <section id="skills" className="py-16 bg-white">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold text-right text-black">Skills</h2>
+          <ul className="mt-4">
+            <li>
+              <h3 className="text-lg font-bold text-right text-black">Frontend Development</h3>
+              <p className="text-right text-black">HTML, CSS, JavaScript, React, Next.js</p>
+            </li>
+            <li>
+              <h3 className="text-lg font-bold text-right text-black">Mobile Development</h3>
+              <p className="text-right text-black">React Native, Flutter, Kotlin</p>
+            </li>
+            <li>
+              <h3 className="text-lg font-bold text-right text-black">Backend Development</h3>
+              <p className="text-right text-black">Node.js, Express, MongoDB</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      {/* Create experience section, use slider for companies */}
+      <section id="experience" className="bg-gray-100 py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold">Experience</h2>
+          <div className="mt-4">
+            <div className="flex space-x-4">
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold">Frontend Engineer</h3>
+                <p>PT IDStar Cipta Teknologi</p>
+              </div>
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold">React Native Engineer</h3>
+                <p>PT Telkom Indonesia</p>
+              </div>
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold">React Native Engineer</h3>
+                <p>PT Anabatic Teknologies Tbk</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Create project section */}
+      <section id="projects" className="py-6 bg-white">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-bold text-black">Projects</h2>
+          <div className="mt-4">
+            <div className="flex space-x-4">
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold text-black">Arjuna</h3>
+                <p className="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae officia, iure laudantium quas quis expedita! Assumenda, cumque doloribus? Maiores deserunt reiciendis excepturi alias temporibus, dolorem ad vel dicta distinctio?</p>
+              </div>
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold text-black">Bonum POS</h3>
+                <p className="text-black">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda aliquid velit placeat, necessitatibus deserunt quis officiis aliquam dignissimos nemo facilis excepturi in, doloribus maiores est quo commodi tempore pariatur officia.</p>
+              </div>
+              <div className="w-1/3">
+                <h3 className="text-lg font-bold text-black">D-Wallet by Danamon</h3>
+                <p className="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aspernatur voluptates nesciunt soluta praesentium, a aliquam. Animi, illum. Voluptatem fuga cupiditate dignissimos? Deserunt, ratione quae laudantium tenetur perferendis voluptates omnis?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Create footer section, add social media typography */}
+      <footer id="footer" className="bg-black py-4 w-full">
+        <div className="container mx-auto px-4">
+          <ul className="flex justify-end space-x-4">
+            <li>
+              <a href="https://github.com/mahardikaindra" className="text-white hover:text-[#757575]">Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/raka-mahardika-792462121/" className="text-white hover:text-[#757575]">LinkedIn</a>
+            </li>
+          </ul>
+        </div>  
+        <div className="container mx-auto px-4">
+          <p className="text-[#f5f5f5] text-center">© 2021 Raka Mahardika. All rights reserved.</p>
+        </div>
+      </footer>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,14 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // custom warna pallet dengan kode warna berikut:
+  // Contoh Palet:
+  // #FF0000 (Merah Bendera Indonesia)
+  // #FFFFFF (Putih)
+  // #F5F5F5 (Abu-abu Terang)
+  // #212121 (Hitam)
+  // #FFCCCC (Merah Muda Terang)
+  // #757575 (Abu-abu Tua)
   theme: {
     extend: {
       backgroundImage: {
@@ -13,6 +21,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      "red-flag": "#FF0000",
+      white: "#FFFFFF",
+      "gray-light": "#F5F5F5",
+      black: "#212121",
+      "red-light": "#FFCCCC",
+      "gray-dark": "#757575",
     },
   },
   plugins: [],
